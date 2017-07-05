b7e65c59f72fe59c558ab43c8125c40d11d044ad
Jaison <jaisonsaji@gmail.com>
2017-07-05 12:49:52 +0530

    install 2to3

--- 2to3.py
+++ 2to3.py
@@ -3,4 +3,5 @@ import sublime_plugin
 import os
 class ConvertPythonCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		os.system("sudo apt-get install 2to3")
 		os.system("2to3 -w "+self.view.file_name())
