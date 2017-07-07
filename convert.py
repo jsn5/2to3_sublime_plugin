f8004880fa662ae4e1ed3deb58ea226b1174c83e
Jaison <jaisonsaji@gmail.com>
2017-07-07 09:38:15 +0530

    bug fixes

--- convert.py
+++ convert.py
@@ -3,4 +3,9 @@ import sublime_plugin
 import os
 class ConvertPythonCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		os.system("python 2to3.py -w "+self.view.file_name())
+		if self.view.file_name() == None:
+			sublime.error_message("Save the file first to perform the conversion.\n")
+		else:
+			self.view.run_command('save')
+			os.system("python 2to3.py -w "+self.view.file_name())
+			
